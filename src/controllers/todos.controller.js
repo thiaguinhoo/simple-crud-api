d22e59f7cdfd3cda27855529768fab011b4c7705
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-28 15:00:00 -0300

    feat: implementando todos controller e todos routes

--- src/controllers/todos.controller.js
+++ src/controllers/todos.controller.js
@@ -1,8 +1,42 @@
 /* eslint-disable prettier/prettier */
 const { StatusCodes } = require('http-status-codes');
 
+
+
+// eslint-disable-next-line no-unused-vars
+const User = require('../models/User')
+const Todos = require('../models/Todos')
+// eslint-disable-next-line no-unused-vars
+const Tarefa = require('../models/Tarefas')
+
 module.exports = {
   all: async (_, response) => {
+    const list = await  Todos.findAll()
+    response.json({list})
     response.sendStatus(StatusCodes.UNAUTHORIZED);
   },
+  // eslint-disable-next-line no-unused-vars
+
+
+  criarLista: async (request, response) =>{
+    Todos.create({titulo: request.body.titulo})
+    .then(todos => {
+     response.json((todos));
+    });
+
+  },
+
+  deletarLista:(request,response)=>{
+    var id = request.id.params
+    let todo = Todos.findByPk(id)
+    if(todo){
+         todo.destroy()
+    }
+    response.json({})
+  }
+ 
+
+
+
+
 };
--- src/routes/todos.routes.js
+++ src/routes/todos.routes.js
@@ -1,9 +1,18 @@
 /* eslint-disable prettier/prettier */
-const { Router } = require('express');
+const express = require('express')
+const router = express.Router()
 const todosController = require('../controllers/todos.controller');
 
-const router = Router();
+
+
+router.get("/ping",(req,res) =>{
+    res.json({pong:true})
+})
 
 router.route('/').get(todosController.all);
+
+router.route('/new').post(todosController.criarLista)
+
+router.route('/delete/:id',todosController.deletarLista)
 
 module.exports = router;
