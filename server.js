de792417eeb090805f89b5f507b7397b6de7d275
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-28 10:08:59 -0300

    feat: adicionando verificação de conexão com o banco

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 /* eslint-disable prettier/prettier */
 const express = require('express');
 const todosRoutes = require('./src/routes/todos.routes');
-
+const connection = require('./src/database/database')
 const application = express();
+
+
+
+connection 
+        .authenticate()
+        .then(()=>{
+            console.log("Db connectado"); 
+        }).catch((error) => {
+            console.log(error)      
+        })
+
 
 application.use('/todos', todosRoutes);
 
