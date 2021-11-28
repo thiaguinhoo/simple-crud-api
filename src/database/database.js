3c37dadda8f4461ae19ca40936d788417d18b49f
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-28 14:57:48 -0300

    feat: tirando o timestamp das tabelas

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,16 @@
 /* eslint-disable prettier/prettier */
-const sequelize = require("sequelize");
+
+const sequelize = require("sequelize")
 
 
 const connection = new sequelize('todo','root','root',{
         host: 'localhost',
-        dialect: 'mysql'
-        });
+        dialect: 'mysql',
+        timestamps: false
+
+});
 
 
 module.exports = connection ;
+
+
