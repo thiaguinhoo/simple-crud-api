315f3ad1bf920cdc7872a4535893781f16f1bb32
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-12-06 03:12:02 -0300

    feat: implementando os métodos listarUsers, criarUsers e deletarUser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,7 @@
 /* eslint-disable prettier/prettier */
-const User = require('../models/User')
-// eslint-disable-next-line no-unused-vars
-const Todos = require('../models/Todos')
-// eslint-disable-next-line no-unused-vars
-const Tarefa = require('../models/Tarefas');
+
+const db = require("../models");
+
 var bcrypt = require('bcrypt')
 const { StatusCodes } = require('http-status-codes');
 
@@ -11,24 +9,24 @@ module.exports = {
 
     
     listarUsers: async (_, response) => {
-                let users = await User.findAll()
+                let users = await db.User.findAll()
                 response.json({users})
                 response.sendStatus(StatusCodes.OK);
       
     },
 
     criarUser: async (request, response) =>{
-        let name = request.body.name
+        let username = request.body.username
         let password = request.body.password
         if(password.length < 6){
             response.json({error: 'Password precisa de mais de 6 dígitos'})
         }
 
-        else if(name != undefined && password != undefined){
+        else if(username != undefined && password != undefined){
             var salt = bcrypt.genSaltSync(10)
             var hash = bcrypt.hashSync(password,salt)
-            User.create({
-                        name: name,
+            db.User.create({
+                        username: username,
                         password: hash
                         
                 }).then(()=>{
@@ -43,12 +41,15 @@ module.exports = {
         const id = parseInt(request.params.id);
         if (Number.isNaN(id)) return response.status(400).end();
        
-        User.destroy({
+        db.User.destroy({
          where: {id}
         }).then(() => {
          response.status(204).end();
         }); 
        },
+
+  
+       
 
 
 }
